72aa17fd8f0459c4c3a1d536b867b5db698e0b19
hsuchingchun <48717305+hsuchingchun@users.noreply.github.com>
2022-10-20 18:23:19 +0800

    1020

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', event => {
+$(() => {
     $('#mainNav').css('visibility', 'visible')
         // Navbar shrink function
     var navbarShrink = function() {
@@ -33,4 +33,47 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+
+    // project-filter
+    $('.filter h6').filter('.all').addClass('filter-border')
+
+    $('.filter').on('click', '.all', function() {
+        $('.filter-border').removeClass('filter-border');
+        $('.filter h6').filter('.all').addClass('filter-border')
+
+        $('.hide-filter').removeClass('hide-filter');
+    });
+    $('.filter').on('click', '.tech-art', function() {
+        $('.filter-border').removeClass('filter-border');
+        $('.filter h6').filter('.tech-art').addClass('filter-border')
+
+        $('.hide-filter').removeClass('hide-filter');
+        $('#project li').not('.tech-art').addClass('hide-filter');
+    });
+    $('.filter').on('click', '.art-show', function() {
+        $('.filter-border').removeClass('filter-border');
+        $('.filter h6').filter('.art-show').addClass('filter-border')
+
+        $('.hide-filter').removeClass('hide-filter');
+        $('#project li').not('.art-show').addClass('hide-filter');
+    });
+    $('.filter').on('click', '.interactive', function() {
+        $('.filter-border').removeClass('filter-border');
+        $('.filter h6').filter('.interactive').addClass('filter-border')
+
+
+        $('.hide-filter').removeClass('hide-filter');
+        $('#project li').not('.interactive').addClass('hide-filter');
+    });
+    $('.filter').on('click', '.co-play', function() {
+        $('.filter-border').removeClass('filter-border');
+        $('.filter h6').filter('.co-play').addClass('filter-border')
+
+
+        $('.hide-filter').removeClass('hide-filter');
+        $('#project li').not('.co-play').addClass('hide-filter');
+    });
+
+
 });
